fb978fc7062bc10cf7a8939840b9a19a28084b78
anuragrao04 <raoanu2004@gmail.com>
2021-01-31 22:32:48 +0530

    tweaked login-register page and solved loadUI issue in main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import sys 
 from PyQt5.uic import loadUi
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QDialog, QApplication
+from PyQt5.QtWidgets import QDialog, QApplication, QMainWindow
 
 
 
-class MainWindow(QDialog):
+class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         loadUi("loginRegisterPage.ui",self)
@@ -17,20 +17,24 @@ class MainWindow(QDialog):
 
 
 
-class login_page(QDialog):
+class login_page(QMainWindow):
     def __init__(self):
         super(login_page,self).__init__()
-        loadUi("loginPage",self)
+        loadUi("loginPage.ui",self)
 
 
 
 #main
 app = QApplication(sys.argv)
 widget = QtWidgets.QStackedWidget()
+widget.setFixedWidth(800)
+widget.setFixedHeight(600)
 main_window = MainWindow()
 loginpage = login_page()
 widget.addWidget(main_window)
 widget.addWidget(loginpage)
+widget.show()
+
 
 
 
