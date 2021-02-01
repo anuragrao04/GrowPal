1f895d9fd7bb9922a1e6e5ba3f53da264efed9f3
anuragrao04 <raoanu2004@gmail.com>
2021-02-01 13:05:13 +0530

    Added functionality of not accepting empty text fields

--- main.py
+++ main.py
@@ -40,9 +40,12 @@ class register_page(QMainWindow):
     def __init__(self):
         super(register_page, self).__init__()
         loadUi("registerPage.ui", self)
-        self.pushbutton_register.clicked.connect(self.gotologin_register_page)
-    def gotologin_register_page(self):
-        widget.setCurrentIndex(widget.currentIndex()-2)
+        self.pushbutton_register.clicked.connect(self.register_button_pressed)
+    def register_button_pressed(self):
+        if self.lineEdit_email.text() == "" or self.lineEdit_phnumber.text() == "" or self.lineEdit_password.text() == "" or self.lineEdit_repeatpassword.text() == "":
+            print("empty")
+        else:
+            widget.setCurrentIndex(widget.currentIndex()-2)
 
 
 #main
