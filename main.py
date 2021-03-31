325b3c94c9493f965d4adb9fdf0669dd93e797fa
anuragrao04 <raoanu2004@gmail.com>
2021-03-31 19:14:11 +0530

    refined comments

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 # Import statements:
-import sys  # why sys library? it helps to execute the main GUI window
-from PyQt5.uic import loadUi # Why loadUI? used to load the .ui files to the main execution flow
+import sys  
+from PyQt5.uic import loadUi 
 from PyQt5 import QtWidgets 
 from PyQt5.QtWidgets import QDialog, QApplication, QMainWindow, QLineEdit
 from validate_email import validate_email
-
-# PyQt5 -> framework for creating GUI. QDialog, QApplication, etc are just classes of the PyQt5 library
-
-# End of import statements
 
 
 # Variables:
@@ -17,28 +13,28 @@ global register_page_email
 global register_page_password
 
 
-# variable not really used, just experimenting
+
 
 # Class declaration for all pages:
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
-        loadUi("loginRegisterPage.ui",self) # loads the .ui file
+        loadUi("loginRegisterPage.ui",self) 
         self.setWindowTitle("GrowPal")
-        self.login_button.clicked.connect(self.gotologin_page) # on button click, executes the function gotologin_page()
-        self.register_button.clicked.connect(self.gotoregister_page) # on button click, executes the function gotoregister_page()
+        self.login_button.clicked.connect(self.gotologin_page) 
+        self.register_button.clicked.connect(self.gotoregister_page) 
 
     def gotologin_page(self):
-        widget.setCurrentIndex(1) # Index manipulation to change the stacked screens
+        widget.setCurrentIndex(1) 
     def gotoregister_page(self):
-        widget.setCurrentIndex(2) # Same Index manipulation stuff
+        widget.setCurrentIndex(2)
 
 class login_page(QMainWindow):
     def __init__(self):
         super(login_page,self).__init__() 
-        loadUi("loginPage.ui",self) # loads the .ui file
+        loadUi("loginPage.ui",self) 
         self.pushButton_back.clicked.connect(self.back_button_pressed)
-        self.pushbutton_login.clicked.connect(self.login_button_pressed) # button thing. directs to login_button_pressed()
+        self.pushbutton_login.clicked.connect(self.login_button_pressed) 
         self.password_view.clicked.connect(self.pass_view_clicked)
 
     def pass_view_clicked(self):
@@ -50,11 +46,11 @@ class login_page(QMainWindow):
     def back_button_pressed(self):
         widget.setCurrentIndex(0)
     def login_button_pressed(self):
-        if self.lineEdit_email.text() == "" or self.lineEdit_password.text() == "": # checks if there are any empty fields
+        if self.lineEdit_email.text() == "" or self.lineEdit_password.text() == "": 
             print("empty")
         else:
-            if self.lineEdit_email.text() in loginpage_details.keys():      # checking if username is in the dictionary. the .keys() function returns all the key values
-                if self.lineEdit_password.text() == loginpage_details[self.lineEdit_email.text()]:     # checking if the password matches the username
+            if self.lineEdit_email.text() in loginpage_details.keys():     
+                if self.lineEdit_password.text() == loginpage_details[self.lineEdit_email.text()]:     
                     self.lineEdit_email.setText("")
                     self.lineEdit_password.setText("")
                     widget.setCurrentIndex(3)
@@ -66,7 +62,7 @@ class login_page(QMainWindow):
             else:
                 error_dialog = QtWidgets.QErrorMessage(self)
                 error_dialog.setWindowTitle('Account')
-                error_dialog.showMessage('Please create an account')                   # if username is not in the dictionary
+                error_dialog.showMessage('Please create an account')                  
                 self.lineEdit_email.setText("")
                 self.lineEdit_password.setText("")
                 widget.setCurrentIndex(2)
@@ -74,9 +70,9 @@ class login_page(QMainWindow):
 class register_page(QMainWindow):
     def __init__(self):
         super(register_page, self).__init__()
-        loadUi("registerPage.ui", self) # loads the .ui file
+        loadUi("registerPage.ui", self) 
         self.pushButton_back.clicked.connect(self.back_button_pressed)
-        self.pushbutton_register.clicked.connect(self.register_button_pressed) # button thing again
+        self.pushbutton_register.clicked.connect(self.register_button_pressed) 
         self.sp_view.clicked.connect(self.sp_view_clicked)
         self.cp_view.clicked.connect(self.cp_view_clicked)
 
@@ -97,7 +93,7 @@ class register_page(QMainWindow):
     def register_button_pressed(self):
 
         if self.lineEdit_username.text() == "" or self.lineEdit_email.text() == "" or self.lineEdit_phnumber.text() == "" or self.lineEdit_password.text() == "" or self.lineEdit_repeatpassword.text() == "":
-            print("empty")   # checks for empty fields
+            print("empty")  
 
         elif len(self.lineEdit_phnumber.text()) != 10:
             error_dialog = QtWidgets.QErrorMessage(self)
@@ -123,10 +119,10 @@ class register_page(QMainWindow):
                 self.lineEdit_password.setText("")
                 self.lineEdit_repeatpassword.setText("")
 
-        elif self.lineEdit_password.text() != self.lineEdit_repeatpassword.text(): # if those two fields do not match, display an error box
+        elif self.lineEdit_password.text() != self.lineEdit_repeatpassword.text(): 
             error_dialog = QtWidgets.QErrorMessage(self)
-            error_dialog.setWindowTitle('Password') # Window title for the error box
-            error_dialog.showMessage('Your passwords do not match. Try again.') # message in the error box
+            error_dialog.setWindowTitle('Password') 
+            error_dialog.showMessage('Your passwords do not match. Try again.') 
             self.lineEdit_password.setText("")
             self.lineEdit_repeatpassword.setText("")
 
@@ -143,11 +139,11 @@ class buy_page(QMainWindow):
 
 # Indexing for stacked widget:
 app = QApplication(sys.argv)
-widget = QtWidgets.QStackedWidget() # stacked widget
+widget = QtWidgets.QStackedWidget() 
 
 
-main_window = MainWindow() # setting the classes as a variable for QStacked widget
-loginpage = login_page() # same thing as above
+main_window = MainWindow() 
+loginpage = login_page() 
 buypage = buy_page()
 registerpage = register_page()
 # Indexing for all the stacked pages. indexes are appointed in the order they are added.
@@ -162,7 +158,7 @@ widget.addWidget(buypage)     # 3
 widget.show()
 
 
-# Exit - terminates the application when the x button is clicked
+# Exit 
 
 try:
     sys.exit(app.exec_())
